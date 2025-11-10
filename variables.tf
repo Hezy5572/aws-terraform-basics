eb2a322adaf2b63d39187fab8651d71d7e3b4109
Hezy5572 <aoheze@gmail.com>
2025-11-10 12:29:23 -0500

    First change in October

--- variables.tf
+++ variables.tf
@@ -1,8 +1,9 @@
 variable "aws_region" {
-  default = "us-east-1"
+  default = "ca-central-1"
 }
 
 variable "bucket_name" {
   default = "Adeyemi-s3-bucket-98765447839821"  # Change to a unique name!
 }
+
 
